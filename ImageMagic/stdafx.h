8fb849715f4448aa896c98e90975016475bba7d2
Neo-T <marsstory99@qq.com>
2018-07-30 17:54:17 +0800

    Not to be complete

--- ImageMagic/stdafx.h
+++ ImageMagic/stdafx.h
@@ -10,12 +10,7 @@
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
 // Windows 头文件: 
 #include <windows.h>
-
-// C 运行时头文件
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#include <wtypes.h>
 
 
 // TODO:  在此处引用程序需要的其他头文件
